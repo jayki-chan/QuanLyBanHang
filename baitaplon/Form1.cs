4fdf4382310653292ffc6fe533e725d68c76626c
LINH <nguyenthikhanhlinh2200545@viu.edu.vn>
2026-03-09 11:28:16 +0700

    Add files via upload

---
+++ baitaplon/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baitaplon
+{
+    public partial class frmMain : Form
+    {
+       public void xemDanhMuc (int intDanhMuc)
+        {
+            XemDM tp = new XemDM ();
+            tp.Text = intDanhMuc.ToString ();
+            tp.ShowDialog();
+        }
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        private void danhMụcThànhPhốToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            xemDanhMuc(1);
+        }
+        private void danhMụcKháchHàngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            xemDanhMuc(2);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Dangnhap Dangnhap = new Dangnhap();
+            Dangnhap.FormClosed += Dangnhap_FormClosed;
+            Dangnhap.ShowDialog();
+            this.WindowState = FormWindowState.Normal;
+        }
+        private void danhMụcThànhPhoToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Danhmucthanhpho dmtp = new Danhmucthanhpho();
+            dmtp.Text = "Quản Lý Danh Mục Thành Phố";
+            dmtp.ShowDialog();
+        }
+        private void danhMụcSảnPhẩmToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            xemDanhMuc(4);
+        }
+         private void danhMụcHóaĐơnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            xemDanhMuc(5);
+        }
+
+        private void danhMụcChiTiếtHóaĐơnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            xemDanhMuc(6);
+        }
+        private void xemDanhMụcToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void mậtKhẩuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void quanToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void đăngNhậpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private void Dangnhap_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.menuStrip1.Enabled = true;
+        }
+
+        private void danhMụcNhânViênToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            xemDanhMuc(3);
+        }
+    }
+}
---
+++ baitaplon/XemDM.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baitaplon
+{
+    public partial class XemDM : Form
+    {           
+        string ketnoi = "Data Source=LAPTOP-V3HRC9O8\\MSSQLSERVER01; Database=quanlybanhang; User Id=sa; Password=123456 ";
+        SqlConnection conn = null;
+        SqlDataAdapter daTable = null;
+        DataTable dtTable = null;
+        public XemDM()
+        {
+            InitializeComponent();
+        }
+        private void XemDM_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SqlConnection(ketnoi);
+                int intDM = Convert.ToInt32(this.Text);
+                switch (intDM)
+                {
+                    case 1:
+                        this.Text = "Danh Mục Thành Phố";
+                        lblDanhmuc.Text = "Danh Mục Thành Phố";
+                        daTable = new SqlDataAdapter("SELECT * FROM thanhpho", conn);
+                        break;
+                    case 2:
+                        this.Text = "Danh Mục Khách Hàng";
+                        lblDanhmuc.Text = "Danh Mục Khách Hàng";
+                        daTable = new SqlDataAdapter("SELECT * FROM khachhang", conn);
+                        break;
+                    case 3:
+                        this.Text = "Danh Mục Nhân Viên";
+                        lblDanhmuc.Text = "Danh Mục Nhân Viên";
+                        daTable = new SqlDataAdapter("SELECT * FROM nhanvien", conn);
+                        break;
+                    case 4:
+                        this.Text = "Danh Mục Sản Phẩm";
+                        lblDanhmuc.Text = "Danh Mục Sản Phẩm";
+                        daTable = new SqlDataAdapter("SELECT * FROM SanP", conn);
+                        break;
+                    case 5:
+                        this.Text = "Danh Mục Hóa Đơn";
+                        lblDanhmuc.Text = "Danh Mục Hóa Đơn";
+                        daTable = new SqlDataAdapter("SELECT * FROM hoadon", conn);
+                        break;
+                    case 6:
+                        this.Text = "Danh Mục Chi Tiết Hóa Đơn";
+                        lblDanhmuc.Text = "Danh Mục Hóa Đơn";
+                        daTable = new SqlDataAdapter("SELECT * FROM chitiethoadon", conn);
+                        break;
+                    default:
+                        break;
+                }
+                dtTable = new DataTable();
+                dtTable.Clear();
+                daTable.Fill(dtTable);
+                DataGridView1.DataSource = dtTable;
+                DataGridView1.AutoResizeColumns();
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Không lấy được nội dung trong table. Lỗi rồi!!!");
+            }
+        }
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
